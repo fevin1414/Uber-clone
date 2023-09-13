d597c3e333bef4c08b82da1c78b2fc7896f17f68
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 18:46:58 -0500

    added store and slices

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+import { store } from './store';
 
 
 
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
-      <Text>Lets Build Uber</Text>
+      <Text>Lets Build Uber!</Text>
       <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
