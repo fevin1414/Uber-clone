7b5a220703c4872663155454efd6558db5b8d676
fevin1414 <fevinbiju1414@gmail.com>
2023-09-13 21:56:43 -0500

    adding maps

---
+++ uber-clone/components/Map.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import MapView from 'react-native-maps';
+const Map = () => {
+  return (
+    <View>
+      <Text>I'm a Map</Text>
+    </View>
+  )
+}
+
+export default  Map
+
+const styles = StyleSheet.create({})
--- uber-clone/screens/HomeScreen.js
+++ uber-clone/screens/HomeScreen.js
@@ -4,8 +4,11 @@ import tw from "twrnc";
 import NavOptions from "../components/NavOptions";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination,setOrigin } from "../slices/navSlice";
 
 const HomeScreen = () => {
+  const dispatch=useDispatch();
   return (
     <SafeAreaView style={tw`bg-white h-full`}>
       <View style={tw`p-5`}>
@@ -30,6 +33,18 @@ const HomeScreen = () => {
             fontSize:18,
           },
         }}
+        onPress={(data,details=null)=>{
+   dispatch(setOrigin({
+    location:details.geometry.location,
+    description:data.description
+   }))
+   dispatch(setDestination(null))
+
+        }}
+        fetchDetails={true}
+        returnKeyType={"search"}
+        enablePoweredByContainer={false}
+        minLength={2}
         query={{
           key:GOOGLE_MAPS_APIKEY,
           language:"en",
