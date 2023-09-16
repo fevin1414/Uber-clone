78de88ab11e309ab16fb3a7f3972602e6a505810
fevin1414 <fevinbiju1414@gmail.com>
2023-09-16 01:17:34 -0500

    added marker

--- uber-clone/components/Map.js
+++ uber-clone/components/Map.js
@@ -9,16 +9,26 @@ const Map = () => {
   const origin=useSelector(selectOrigin);
   return (
 <MapView
-style={tw`flex-1`}
-mapType='mutedStandard'
+  style={tw`flex-1`}
+  mapType='mutedStandard'
   initialRegion={{
     latitude:origin.location.lat,
     longitude: origin.location.lng,
     latitudeDelta: 0.005,
     longitudeDelta: 0.005,
   }}
-/>
-  )
+>
+  {origin?.location && (
+    <Marker
+      coordinate={{
+        latitude: origin.location.lat,
+        longitude: origin.location.lng,
+      }}
+    />
+  )}
+</MapView>
+
+  );
 }
 
 export default  Map
