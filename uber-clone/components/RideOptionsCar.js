d9be508d0b1fb135b0b70c3a3bee210edbc67f96
fevin1414 <fevinbiju1414@gmail.com>
2023-09-18 11:11:05 -0500

    added ride options

--- uber-clone/components/RideOptionsCar.js
+++ uber-clone/components/RideOptionsCar.js
@@ -1,10 +1,34 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import { FlatList, StyleSheet, Text, TouchableOpacity, View , Image,} from 'react-native'
 import React from 'react'
 import tw from "tailwind-react-native-classnames";
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { Icon } from "react-native-elements";
 import { useNavigation } from '@react-navigation/native';
 
+const data=[
+  {
+    id:"uber-X-123",
+    title:"UberX",
+    multiplier:1,
+    image:"https://links.papareact.com/3pn"
+  },
+  {
+    id:"uber-XL-456",
+    title:"Uber XL",
+    multiplier:1.2,
+    // image:"https://links.papareact.com/5wn"
+    image:"https://links.papareact.com/3pn"
+
+  },
+  {
+    id:"uber-LUX-789",
+    title:"Uber LUX",
+    multiplier:1.75,
+    // image:"https://links.papareact.com/7pfn"
+    image:"https://links.papareact.com/3pn"
+
+  },
+]
 const RideOptionsCar = () => {
   const navigation=useNavigation();
   return (
@@ -16,6 +40,25 @@ const RideOptionsCar = () => {
         </TouchableOpacity>
       <Text style={tw `text-center py-5 text-xl`}>Select A Ride</Text>
       </View>
+      <FlatList data={data} keyExtractor={(item)=>item.id}
+      renderItem={({item:{id,title,multiplier,image},item})=>(
+<TouchableOpacity>
+<Image
+style={{
+  width:100,
+  height:100,
+  resizeMode:"contain"
+}}
+source={{uri:image}}
+/>
+<View>
+  <Text>{title}</Text>
+  <Text>Travel Time ..</Text>
+
+</View>
+</TouchableOpacity>
+      )}
+      />
     </SafeAreaView>
   )
 }
