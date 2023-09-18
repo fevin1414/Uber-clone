dfd532efef6ccbeb6df598d49e686f0a44e27b32
fevin1414 <fevinbiju1414@gmail.com>
2023-09-18 09:15:15 -0500

    added pointers

--- uber-clone/components/Map.js
+++ uber-clone/components/Map.js
@@ -1,5 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 import MapView,{Marker} from 'react-native-maps';
 import tw from "twrnc";
 import MapViewDirections from 'react-native-maps-directions';
@@ -10,16 +10,24 @@ import { selectDestination, selectOrigin } from '../slices/navSlice';
 
 const Map = () => {
   const origin=useSelector(selectOrigin);
-  const destination=useSelector(selectDestination)
+  const destination=useSelector(selectDestination);
+  const mapRef=useRef(null);
+  useEffect(()=>{
+    if(!origin|| ! destination) return;
+    mapRef.current.fitToSuppliedMarkers(["origin","destination"],{edgePadding:{
+    top:50,right:50,bottom:50,left:50
+    }})
+  },[origin,destination])
   return (
 <MapView
+ref={mapRef}
   style={tw`flex-1`}
   mapType='mutedStandard'
   initialRegion={{
     latitude:origin.location.lat,
     longitude: origin.location.lng,
     latitudeDelta: 0.005,
-    longitudeDelta: 0.025,
+    longitudeDelta: 0.005,
   }}
 >
   {origin&&destination && (
@@ -40,6 +48,17 @@ const Map = () => {
       title="origin"
       description={origin.description}
       identifier="origin"
+    />
+  )}
+   {destination?.location && (
+    <Marker
+      coordinate={{
+        latitude: destination.location.lat,
+        longitude: destination.location.lng,
+      }}
+      title="destination"
+      description={destination.description}
+      identifier="destination"
     />
   )}
 </MapView>
