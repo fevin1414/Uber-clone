d28a80e5329ad386e61d09782cbaf165db053f7e
fevin1414 <fevinbiju1414@gmail.com>
2023-09-18 10:33:35 -0500

    added rides and eats

--- uber-clone/components/NavigateCard.js
+++ uber-clone/components/NavigateCard.js
@@ -1,67 +1,88 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
 import tw from "tailwind-react-native-classnames";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { Icon } from "react-native-elements";
 
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { useDispatch } from 'react-redux';
-import { setDestination } from '../slices/navSlice';
-import { useNavigation } from '@react-navigation/native';
-import NavFavourites from './NavFavourites';
+import { SafeAreaView } from "react-native-safe-area-context";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlice";
+import { useNavigation } from "@react-navigation/native";
+import NavFavourites from "./NavFavourites";
 
 const NavigateCard = () => {
-  const navigation=useNavigation();
-  const dispatch=useDispatch();
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
   return (
-    <SafeAreaView style={tw `bg-white flex-1`}>
+    <SafeAreaView style={tw`bg-white flex-1`}>
       <Text style={tw`text-center py-5 text-xl`}>Hello</Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
- <GooglePlacesAutocomplete
- placeholder="where to ?"
- styles={toInputBoxStyles}
- fetchDetails={true}
- returnKeyType={"search"}
- enablePoweredByContainer={false}
- minLength={2}
- onPress={(data,details=null)=>{
-dispatch(setDestination({
-  location:details.geometry.location,
-  description:data.description,
-}));
-navigation.navigate('RideOptionsCar');
-
- }}
- query={{
-  key:GOOGLE_MAPS_APIKEY,
-  language:"en",
-}}
- nearbyPlacesAPI='GooglePlacesSearch'
- debounce={400}
- />
+          <GooglePlacesAutocomplete
+            placeholder="where to ?"
+            styles={toInputBoxStyles}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            enablePoweredByContainer={false}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCar");
+            }}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+            nearbyPlacesAPI="GooglePlacesSearch"
+            debounce={400}
+          />
         </View>
         <NavFavourites />
       </View>
+      <View>
+        <TouchableOpacity
+          style={tw`flex flex-row bg-black w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon name="car" type="font-awesome" color="white" size={16} />
+          <Text style={tw`text-white text-center`}>Rides</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={tw`flex flex-row w-24 px-4 py-3 rounded-full`}
+        >
+          <Icon
+            name="fast-food-outline"
+            type="ionicon"
+            color="black"
+            size={16}
+          />
+          <Text style={tw` text-center`}>Eats</Text>
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default NavigateCard
+export default NavigateCard;
 
 const toInputBoxStyles = StyleSheet.create({
-  container:{
-    backgroundColor:"white",
-    paddingTop:20,
-    flex:0
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
   },
-  textInput:{
-    backgroundColor:"#DDDDDF",
-    borderRadius:0,
-    fontSize:18,
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
   },
-  textInputContainer:{
-    paddingHorizontal:20,
-    paddingBottom:0,
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
   },
 });
