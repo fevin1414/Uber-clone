137e77c4d5d3b31a608be33e7f38454a24697465
fevin1414 <fevinbiju1414@gmail.com>
2023-09-18 10:40:05 -0500

    added navigation

--- uber-clone/components/NavigateCard.js
+++ uber-clone/components/NavigateCard.js
@@ -10,6 +10,7 @@ import { useDispatch } from "react-redux";
 import { setDestination } from "../slices/navSlice";
 import { useNavigation } from "@react-navigation/native";
 import NavFavourites from "./NavFavourites";
+import RideOptionsCar from "./RideOptionsCar";
 
 const NavigateCard = () => {
   const navigation = useNavigation();
@@ -45,15 +46,16 @@ const NavigateCard = () => {
         </View>
         <NavFavourites />
       </View>
-      <View>
+      <View style={tw`flex-row bg-white justify-evenly py-2 mt-auto border-t border-gray-100`}>
         <TouchableOpacity
+        onPress={()=>navigation.navigate(RideOptionsCar)}
           style={tw`flex flex-row bg-black w-24 px-4 py-3 rounded-full`}
         >
           <Icon name="car" type="font-awesome" color="white" size={16} />
           <Text style={tw`text-white text-center`}>Rides</Text>
         </TouchableOpacity>
         <TouchableOpacity
-          style={tw`flex flex-row w-24 px-4 py-3 rounded-full`}
+          style={tw`flex flex-row justify-between w-24 px-4 py-3 rounded-full`}
         >
           <Icon
             name="fast-food-outline"
