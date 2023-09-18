6032f5f7a8dbd51ac9524bb5399f8e6f2d8950bb
fevin1414 <fevinbiju1414@gmail.com>
2023-09-18 10:57:27 -0500

    added go back in ridesOption

--- uber-clone/components/RideOptionsCar.js
+++ uber-clone/components/RideOptionsCar.js
@@ -1,12 +1,22 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from "tailwind-react-native-classnames";
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Icon } from "react-native-elements";
+import { useNavigation } from '@react-navigation/native';
 
 const RideOptionsCar = () => {
+  const navigation=useNavigation();
   return (
-    <View>
-      <Text>RideOptionsCar</Text>
-    </View>
+    <SafeAreaView style={tw `bg-white flex-grow`}>
+      <View>
+        <TouchableOpacity onPress={()=>navigation.navigate("NavigateCard")} style={tw `absolute top-3 left-5 p-3 rounded-full z-50`}>
+          <Icon
+          name="chevron-left" type="fontawesome"/>
+        </TouchableOpacity>
+      <Text style={tw `text-center py-5 text-xl`}>Select A Ride</Text>
+      </View>
+    </SafeAreaView>
   )
 }
 
